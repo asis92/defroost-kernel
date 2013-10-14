183810de03e79e513e4d542cbdfd9fd0b9236231
asis_92 <asis.92.12@gmail.com>
2013-10-14 20:25:36 +0200

    GCC 4.7.4 Fix

--- fs/cifs/transport2.c
+++ fs/cifs/transport2.c
@@ -19,8 +19,7 @@
  *   along with this library; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
- 
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+
 #include <linux/fs.h>
 #include <linux/list.h>
 #include <linux/gfp.h>
